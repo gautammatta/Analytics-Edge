6d6f2e99993c808e278f6de07aba0e0574b03830
Gautam Matta <2006.matta@gmail.com>
2017-07-09 23:42:40 -0400

    Completed the changes

--- census.R
+++ census.R
@@ -24,7 +24,7 @@ predLogModel <- predict(logModel, newdata = test, type ='response')
 table(test$over50k, predLogModel >0.5)
 
 table(test$over50k)
- 
+
 ROCRpred <- prediction(predLogModel, test$over50k)
 AUC <- as.numeric(performance(ROCRpred, "auc")@y.values)
 prf <- performance(ROCRpred, measure = "tpr", x.measure = "fpr")
@@ -40,15 +40,64 @@ censusCART <- rpart(train$over50k~., data = train, method = 'class')
 
 prp(censusCART)
 
-predCensusCART <- predict(censusCART, newdata = test)
+predCensusCART <- predict(censusCART, newdata = test, type = 'class')
 
-table(test$over50k, predCensusCART )
+table(test$over50k, predCensusCART)
 
-#ROC <- predict(censusCART, newdata = test)[,2]
 
-ROCRCARTpred <- prediction( predict(censusCART, newdata = test)[,2], test$over50k)
+ROC <- predict(censusCART, newdata = test)[,2]
+
+ROCRCARTpred <- prediction(ROC, test$over50k)
 predCART <- performance(ROCRCARTpred, measure = "tpr", x.measure = "fpr")
 plot(predCART)
 
- 
+
 AUC <- as.numeric(performance(ROCRCARTpred,"auc")@y.values)
+
+
+
+########  A Random Forest Model
+
+set.seed(1)
+
+trainSmall <- train[sample(nrow(train), 2000), ]
+
+library(randomForest)
+
+randomForestModel <- randomForest(over50k~., data = trainSmall )
+
+
+RFPred <- predict(randomForestModel, newdata = test)
+
+table(test$over50k, RFPred)
+
+vu <- varUsed(randomForestModel, count=TRUE)
+vusorted <- sort(vu, decreasing = FALSE, index.return = TRUE)
+
+dotchart(vusorted$x, names(randomForestModel$forest$xlevels[vusorted$ix]))
+
+
+### impurity - most imp variable in terms of mean reduction in impurity! 
+
+varImpPlot(randomForestModel)
+
+######### Selecting cp by Cross-Validation
+library(caret)
+
+library(e1071)
+set.seed(2)
+
+
+numFolds <- trainControl( method = "cv", number = 10 )
+cartGrid <- expand.grid(.cp = seq(0.002,0.1,0.002))
+
+train(over50k ~. , data = train, method = 'rpart' , trControl= numFolds, tuneGrid = cartGrid)
+
+
+
+censusCARTcp <- rpart(over50k ~. , data = train, cp =0.002, method = 'class')
+
+censusCARTcpPred <- predict(censusCARTcp, newdata = test, type = 'class')
+
+table(test$over50k, censusCARTcpPred)
+prp(censusCARTcp)
